b09f2e7f7bdf9bd21d346f83a39381cba851dce5
Gordon B. Isnor <gordonbisnor@gmail.com>
2016-10-07 11:19:48 -0400

    oops

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,11 @@ maintainer_email 'gordonbisnor@gmail.com'
 license          'none'
 description      'Postgres database backups to S3'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.6'
+version          '0.0.7'
 
 recipe "pg-backup", "Postgres backups"
 
-depends 'backup':  '>= 0.0.0'
-depends 'postfix': '>= 0.0.0'
+depends 'backup',  '>= 0.0.0'
+depends 'postfix', '>= 0.0.0'
 
 supports 'ubuntu'
